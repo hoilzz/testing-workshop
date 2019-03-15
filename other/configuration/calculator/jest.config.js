02bf01a5b4c7bfd7d2d5ee66ca9a3f84109bd29d
hoilzz <hoil0221@tmon.co.kr>
2019-03-15 17:38:36 +0900

    css module을 proxy로 잡아서 객체로 반환

---
+++ other/configuration/calculator/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  moduleNameMapper: {
+    // module must come first
+    '\\.module\\.css$': 'identity-obj-proxy',
+    '\\.css$': require.resolve('./test/style-mock.js'),
+    // can also map files that are loaded by webpack with the file-loader
+  },
+}
+
